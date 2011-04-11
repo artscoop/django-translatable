00345df29805cea31e0d19c172b3dad3cca6c290
Mikołaj Siedlarek <mikolaj.siedlarek@gmail.com>
2011-04-11 11:22:49 +0200

    packages VERSION tuple; closes #4

--- tests/package/tests.py
+++ tests/package/tests.py
@@ -7,3 +7,10 @@ class PackageTestCase(unittest.TestCase):
         self.assertRegexpMatches(translatable.__version__, r'^\d+\.\d+\.\d+$',
                                  "Wrong package version format")
 
+    def test_package_version_tuple(self):
+        self.assertIsInstance(translatable.VERSION, tuple)
+        self.assertEquals(len(translatable.VERSION), 3)
+
+    def test_version_string_and_tuple_same(self):
+        self.assertEquals('%d.%d.%d' % translatable.VERSION, translatable.__version__)
+
